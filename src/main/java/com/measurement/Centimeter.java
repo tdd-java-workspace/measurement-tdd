cfff5d75c90db3aaee193f4ddb235cbe8bcf4570
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2024-07-30 16:31:33 +0530

    ADD. Addes tests to check the equality of 1cm and 1cm.

--- src/main/java/com/measurement/Centimeter.java
+++ src/main/java/com/measurement/Centimeter.java
@@ -2,4 +2,26 @@ package com.measurement;
 
 public class Centimeter {
 
+    int magnitude;
+    public Centimeter(int magnitude) {
+        this.magnitude = magnitude;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if(this == object) {
+            return true;
+        }
+        if(object == null || this.getClass() != object.getClass()) {
+            return false;
+        }
+
+        return this.magnitude == ((Centimeter) object).magnitude;
+    }
+
+    @Override
+    public int hashCode() {
+        return Double.hashCode(magnitude);
+    }
+
 }
